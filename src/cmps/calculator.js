32ceecc0d101a30e0c062eaaf5e7bf75704783d0
Sam-Main <sam.joseph.donaldson@gmail.com>
2019-07-15 13:08:43 +1200

    test fix 12

--- src/cmps/calculator.js
+++ src/cmps/calculator.js
@@ -91,10 +91,14 @@ class Calculator extends React.Component {
     //else if (NEG_REG.test(newEq[newEq.length - 1]) && NEG_REG.test(value) && OP_REG.test(newEq[newEq.length - 2]) ) {//Cannot have three operators in a row
        
     //} 
-    else if (OP_REG.test(newEq[newEq.length - 1]) && OP_REG.test(value)) {//Cannot have three operators in a row
+    else if (NEG_REG.test(newEq[newEq.length - 1]) && OP_REG.test(value)) {//Cannot have three operators in a row
       if (OP_REG.test(newEq[newEq.length - 2])) {
         newEq = newEq.slice(0, newEq.length - 2) + value;
-      } else {
+      } else if(NEG_REG.test(value)){
+        newEq = newEq + value;
+        now = value;
+      }
+      else{
         newEq = newEq.slice(0, newEq.length - 1) + value;
       }
       now = value;
